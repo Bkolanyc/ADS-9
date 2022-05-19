3fa724dfb37b5026ee09d6a97e0f704ff47defa4
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-05-19 12:17:38 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,95 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <iostream>
 
+template<typename T>
+class BST
+{
+public:
+    struct Node
+    {
+        T value;
+        int count;
+        Node* left;
+        Node* right;
+    };
+private:
+    Node* root;
+    Node* addNode(Node*, T);
+    int   searchNode(Node*, T);
+    int   heightTree(Node*);
+public:
+    BST();
+    ~BST();
+    void add(T);
+    int  search(T);
+    int height();
+
+};
+template<typename T>
+BST<T>::BST() :root(nullptr) {}
+
+template<typename T>
+BST<T>::~BST()
+{
+}
+template<typename T>
+typename BST<T>::Node* BST<T>::addNode(Node* root, T value) {
+    if (root == nullptr) {
+        root = new Node;
+        root->value = value;
+        root->count = 1;
+        root->left = root->right = nullptr;
+    }
+    else if (root->value > value) {
+        root->left = addNode(root->left, value);
+    }
+    else if (root->value < value) {
+        root->right = addNode(root->right, value);
+    }
+    else
+        root->count++;
+    return root;
+}
+
+template<typename T>
+void BST<T>::add(T value) {
+    root = addNode(root, value);
+}
+
+template<typename T>
+int BST<T>::searchNode(Node* root, T value)
+{
+    if (root == nullptr)
+        return 0;
+    else if (root->value > value)
+        return searchNode(root->left, value);
+    else if (root->value < value)
+        return searchNode(root->right, value);
+    else
+        return root->count;
+}
+
+template<typename T>
+int BST<T>::search(T value)
+{
+    return searchNode(root, value);
+}
+template<typename T>
+int BST<T>::heightTree(Node* root) {
+    if (root == nullptr)
+        return 0;
+    int left = heightTree(root->left);
+    int right = heightTree(root->right);
+    if (left < right)
+        return right + 1;
+    else
+        return left + 1;
+}
+
+template<typename T>
+int BST<T>::height() {
+    return heightTree(root);
+}
 #endif  // INCLUDE_BST_H_
