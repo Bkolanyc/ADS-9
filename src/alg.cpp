38a9a408813dee3eacd545035f928c6602210597
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-05-20 14:31:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,17 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
-using namespace std;
 #include  <locale>
 #include  <cstdlib>
 #include "bst.h"
 #include <fstream>
 
 BST<std::string> makeTree(const char* filename) {
-        ifstream file;
+        std::ifstream file;
         file.open(filename);
-        BST<string> TREE;
-        string word;
-        string line;
-        while (file) { 
+        BST<std::string> TREE;
+        std::string word;
+        std::string line;
+        while (file) {
                 file >> word;
                 int i = 0;
                 while (i < word.size()) {
@@ -27,8 +26,7 @@ BST<std::string> makeTree(const char* filename) {
                         if (k != 0) {
                                 TREE.add(line);
                                 line = "";
-                        }
-                        else
+                        }else
                                 i++;
                 }
         }
