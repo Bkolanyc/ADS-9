2abd2a411912c67fd496fdd9a2bc4e1396608337
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-05-20 14:04:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,34 +8,34 @@ using namespace std;
 
 BST<std::string> makeTree(const char* filename)
 {
-        ifstream file;
-	file.open(filename);
-	BST<string> TREE;
-	string word;
-	string line;
-	while (file)
-	{
-		file >> word;
-		int i = 0;
-		while (i < word.size())
-		{
-			int k = 0;
-			while ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z') && (i < word.size()))
-			{
-				if (word[i] >= 'A' && word[i] <= 'Z')
-					word[i] += 32;
-				line += word[i];
-				i++;
-				k++;
-			}
-			if (k != 0)
-			{
-				TREE.add(line);
-				line = "";
-			}
-			else
-				i++;
-		}
-	}
-	return TREE;
+ifstream file;
+file.open(filename);
+BST<string> TREE;
+string word;
+string line;
+while (file)
+{
+file >> word;
+int i = 0;
+while (i < word.size())
+{
+int k = 0;
+while ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z') && (i < word.size()))
+{
+if (word[i] >= 'A' && word[i] <= 'Z')
+word[i] += 32;
+line += word[i];
+i++;
+k++;
+}
+if (k != 0)
+{
+TREE.add(line);
+line = "";
+}
+else
+i++;
+}
+}
+return TREE;
 }
